d246527af820595ce5c7b691a9dd9061817cd1e6
Barstow <1042108827@mil>
2019-04-26 09:32:53 -0400

    floors html not working

--- SupplyApp/supply/urls.py
+++ SupplyApp/supply/urls.py
@@ -2,6 +2,12 @@ from django.urls import path
 
 from . import views
 
+app_name = 'supply'
+
 urlpatterns = [
     path('', views.index, name='index'),
+    path('<int:barracks_id>/', views.floor, name='floor'),
+    path('<int:barracks_id>/floor_id/', views.bathroom, name='bathroom'),
+    path('<int:barracks_id>/floor_id/bathroom_id', views.statuses, name='statuses'),
+    path('<int:barracks_id>/floor_id/bathroom_id/update', views.update, name='update'),
 ]
--- SupplyApp/supply/views.py
+++ SupplyApp/supply/views.py
@@ -1,5 +1,27 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
+from django.shortcuts import render, get_object_or_404
+from django.shortcuts import get_object_or_404
+
+from .models import Barracks, Floor, Bathroom, Item, Status
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the supply index.")
+    barracks_list = Barracks.objects.all()
+    context = {'barracks_list': barracks_list,}
+    return render(request, 'supply/index.html', context)
+
+def floor(request, barracks_id):
+    barracks = get_object_or_404(Barracks, pk=barracks_id)
+    floor_list = Floor.objects.get(barracks_id=barracks_id)
+    context = {'floor_list': floor_list,}
+    return render(request, 'supply/floor.html', context)
+
+def bathroom(request, floor_id):
+    response = "Select your bathroom on %s."
+    return HttpResponse(response % floor_id)
+
+def statuses(request, bathroom_id):
+    return HttpResponse("These are your supply levels for %s." % bathroom_id)
+
+def update(request, bathroom_id):
+    return HttpResponse("Update any of your bathroom's supply levels")
